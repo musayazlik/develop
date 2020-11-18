8d8510661189c95efb37f67da634296e556b6dd7
Musa Yazlık <musayazlik1997@gmail.com>
2020-11-18 14:40:03 +0300

    Database Information
    Database information has been updated.

--- dv-yonetim/kara-kutu/baglan.php
+++ dv-yonetim/kara-kutu/baglan.php
@@ -1,6 +1,6 @@
 <?php
 try {
-     $db = new PDO("mysql:host=localhost;dbname=develop", "admin1", "");
+     $db = new PDO("mysql:host=localhost;dbname=develop", "root", "");
 } catch ( PDOException $e ){
      print $e->getMessage();
 }
